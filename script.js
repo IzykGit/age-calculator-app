75adcb356f3941d71dd3c31d44b0d4aead2c3767
Izyk <lance_hemphill1234@yahoo.com>
2024-02-24 11:00:08 -0500

    completed

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
 
 
-let dayInput = document.getElementById('day-input');
-let monthInput = document.getElementById('month-input');
-let yearInput = document.getElementById('year-input');
+const dayInput = document.getElementById('day-input');
+const monthInput = document.getElementById('month-input');
+const yearInput = document.getElementById('year-input');
 const inputs = [dayInput, monthInput, yearInput]
 
-let dayResult = document.getElementById('day-result');
-let monthResult = document.getElementById('month-result');
-let yearResult = document.getElementById('year-result');
+const dayResult = document.getElementById('day-result');
+const monthResult = document.getElementById('month-result');
+const yearResult = document.getElementById('year-result');
+
+const dayError = document.getElementById('day-error');
+const monthError = document.getElementById('month-error');
+const yearError = document.getElementById('year-error')
+const errors = [dayError, monthError, yearError]
+
+const calculate = document.getElementById('calculate-button')
 
 let day = 0;
 let month = 0;
 let year = 0
-let currentYear = new Date().getFullYear()
+let currentYear = new Date().getFullYear().toString()
 
-let yearArray = Array.from(String(number), Number)
+
+let daysInMonth;
+
 console.log(currentYear)
-console.log(yearArray)
+
 
 inputs.forEach(input => {
     input.addEventListener("input",  () => {
-        if(input.value >= "0" && input.value <= "9") {
+
+        if(!isNaN(input.value) || input == "Backspace") {
             console.log("this is a number key", input.value)
+
+            input.classList.remove('input-error')
+            input.classList.add('input')
+            errors.forEach(error => error.innerHTML = "")
+
         }          
-        else {
+        else if(isNaN(input)){
             console.log("This is not a number key")
+            errors.forEach(error => error.innerHTML = "Input not a number!")
             input.value = ""
+            input.classList.remove('input')
+            input.classList.add('input-error')
         }
 
         getValues()
@@ -33,38 +51,68 @@ inputs.forEach(input => {
 })
 
 
+
+
 function getValues() { 
     
 
-    if(dayInput.value[0] > 3 || dayInput.value > 31) {
+    if(dayInput.value > 31) {
         console.log("To many days!")
         dayInput.value = ""
+        dayError.innerHTML = "Not a valid day!"
+
+        dayInput.classList.remove('input')
+        dayInput.classList.add('input-error')
     }
     
-    if(monthInput.value[0] > 1 || monthInput.value > 12) {
+    if(monthInput.value > 11) {
        console.log("Too many months!")
        monthInput.value = ""
+       monthError.innerHTML = "Not a valid month!"
+
+       monthInput.classList.remove('input')
+       monthInput.classList.add('input-error')
     }
 
-    if(yearInput.value > 2024) {
+    if(yearInput.value > currentYear) {
         console.log("Too many years")
         yearInput.value = ""
+        yearError.innerHTML = "Must be a past year!"
+
+        yearInput.classList.remove('input')
+        yearInput.classList.add('input-error')
     }
 
-    day = dayInput.value
+
     month = monthInput.value
     year = yearInput.value
-    console.log(day)
-    console.log(month)
-    console.log(year)
+
+    
+    daysInMonth = new Date(year, month + 1, 0).getDate()
+    console.log(daysInMonth)
 }
 
 
 
+function calculateDates() {
+    
+    if(dayInput.value > daysInMonth) {
+        dayInput.value = daysInMonth;
+        dayInput.innerHTML = `${daysInMonth}`
+    }
 
+    const now = new Date()
+    day = now.getDate()
+    month = now.getMonth() + 1
+    year = now.getFullYear()
 
-function daysInMonth(month, year) {
-    return new Date(year, month, 0).getDate();
+    dayResult.innerHTML = day - dayInput.value;
+    monthResult.innerHTML = month - monthInput.value;
+    yearResult.innerHTML = year - yearInput.value;
+
+    
 }
 
-console.log(daysInMonth(1, 1995))
+calculate.addEventListener("click", () => {
+    calculateDates()
+})
